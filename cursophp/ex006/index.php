c06d87d9930567b3212a2dc156bb9b69981ade71
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-03-05 14:45:07 -0400

    Formulário Retroalimetado

---
+++ cursophp/ex006/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php 
+        // Capturando os dados do Formulário Retroalimentado
+        $valor1 = $_GET['v1'] ?? 0;
+        $valor2 = $_GET['v2'] ?? 0; 
+    ?>
+    <main>
+        <h1>Somador de Valores</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="v1">Valor 1</label>
+            <input type="number" name="v1" id="v1" value="<?=$valor1?>">
+            <label for="v2">Valor 2</label>
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>">
+            <input type="submit" value="Somar">
+        </form>
+    </main>
+
+    <section id="res">
+        <h2>Resultado da soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+            print "<p>A soma entre $valor1 e $valor2 é de <strong>$soma</strong></p>";
+        ?>
+    </section>
+</body>
+</html>
