fd66559c28361c0bda60180b5faff4db5d12c2fb
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-06-07 15:01:15 -0400

    created challenge 008

---
+++ cursophp/desafios/desafio008/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    
+    ?>
+    <main>
+        <h1>Informe um número</h1>
+        <p>raiz quadrada e raiz cúbica</p>
+    </main>
+</body>
+</html>
