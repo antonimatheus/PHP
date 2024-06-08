7742fa8f16db13b3a070c9b2fbc9f04863a035a7
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-06-08 19:39:06 -0400

    challenge 008 done

--- cursophp/desafios/desafio008/index.php
+++ cursophp/desafios/desafio008/index.php
@@ -7,12 +7,33 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-    
+    <?php
+        $numero = $_GET['numero'] ?? 0;
     ?>
     <main>
         <h1>Informe um número</h1>
-        <p>raiz quadrada e raiz cúbica</p>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="numero">Número:</label>
+            <input type="number" name="numero" value="<?=$numero?>" id="numero">
+            <input type="submit" value="Executar Raízes">
+        </form>
     </main>
+
+    <?php 
+        $raizQuadrada = sqrt($numero);
+        $raizCubica = $numero ** (1/3);
+        
+        $raizQuadradaFormatada = number_format($raizQuadrada, 3, ",", ".");
+        $raizCubicaFormatada = number_format($raizCubica, 3, ",", ".");
+    ?>
+
+    <section>
+        <h2>Resultado final</h2>
+        <p>Analizando o número <strong><?=$numero?></strong>, temos:</p>
+        <ul>
+            <li>A sua raíz quadrada é <strong><?=$raizQuadradaFormatada?></strong></li>
+            <li>A sua raíz cúbica é <strong><?=$raizCubicaFormatada?></strong></li>
+        </ul>
+    </section>
 </body>
 </html>
