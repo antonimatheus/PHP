bacce51f20c142473eb84f8f30bc37ea9ebb496b
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-06-24 16:46:38 -0400

    challenge 012 done

--- cursophp/desafios/desafio012/index.php
+++ cursophp/desafios/desafio012/index.php
@@ -10,6 +10,7 @@
     <?php
         $tempo = $_GET["tempo"] ?? 0;
     ?>
+
     <main>
         <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
@@ -20,15 +21,29 @@
         </form>
     </main>
 
+    <?php
+        $semanas = ($tempo / 604800);
+        $dias = ($tempo % 604800) / 86400;
+        $horas = ($tempo % 86400) / 3600;
+        $minutos = ($tempo % 3600) / 60;
+        $segundos = ($tempo % 60);
+
+        $semanasF = number_format($semanas, 0);
+        $diasF = number_format($dias, 0);
+        $horasF = number_format($horas, 0);
+        $minutosF = number_format($minutos, 0);
+        $segundosF = number_format($segundos, 0);
+    ?>
+    
     <section>
         <h2>Totalizando tudo</h2>
         <p>Analizando o valor que você digitou, <strong><?=$tempo?> segundos</strong>, equivalem a um total de:</p>
         <ul>
-            <li>... semanas</li>
-            <li>... dias</li>
-            <li>... horas</li>
-            <li>... minutos</li>
-            <li>... segundos</li>
+            <li><?=$semanasF?> semanas</li>
+            <li><?=$diasF?> dias</li>
+            <li><?=$horasF?> horas</li>
+            <li><?=$minutosF?> minutos</li>
+            <li><?=$segundosF?> segundos</li>
         </ul>
     </section>
 </body>
