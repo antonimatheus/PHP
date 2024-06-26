1be037b09b36ee6536d6815289dbe6d5489a14c0
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-06-26 16:07:26 -0400

    created challenge 013

---
+++ cursophp/desafios/desafio013/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Caixa Eletrônico</title>
+</head>
+<body>
+    <main>
+        <?php
+            $valor = $_GET["valor"] ?? 0;
+        ?>
+
+        <h1>Caixa Eletrônico</h1>
+        <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
+            <label for="valor">Qual valor você deseja sacar? (R$)*</label>
+            <input type="number" name="valor" id="valor" value="<?=$valor?>">
+
+            <p>*notas disponíveis: R$100, R$50, R$10, R$5</p>
+
+            <input type="submit" value="Sacar">
+        </form>
+    </main>
+
+    <section>
+        <h2>Saque de R$<?=$valor?> realizado</h1>
+        <p>O caixa eletrônico vai te entragar as seguintes notas:</p>
+        <ul>
+            <li>100: ...</li>
+            <li>50: ...</li>
+            <li>10: ...</li>
+            <li>5: ...</li>
+        </ul>
+    </section>
+</body>
+</html>
